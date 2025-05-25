b0bbc6b784cd26d835494468f08fa8cb50205cb6
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-25 15:12:28 +0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Fazliddin Abdazimov on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
